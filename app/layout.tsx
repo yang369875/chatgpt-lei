1a1950b10ceb5097a33c048574d576ddd18a8679
Leslie Casey <283959666@qq.com>
2023-05-10 08:13:29 +0000

    sunfly修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Chatgpt Sunfly Web",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Chatgpt Sunfly Web",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
